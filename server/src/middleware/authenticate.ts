4ff7c16808d6686cd212823c5702e5d9c8a0dbda
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-16 14:22:52 +0530

    authentication middleware added

---
+++ server/src/middleware/authenticate.ts
@@ -0,0 +1,11 @@
+import { UnAuthenticatedError } from "errors";
+import { Request, Response, NextFunction } from "express";
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.isAuthenticated()) {
+    throw new UnAuthenticatedError("User is not authenticated");
+  }
+  next();
+};
+
+export default authenticate;
