6f63ad3f9f05ec6128efd6b7643e5ef5b2517f57
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-16 12:24:46 +0530

    register controller added

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
+import BadRequestError from "../errors/badRequest.error";
 
 const register = async (req: Request, res: Response) => {
-    res.send("Register Route");
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please fill all the fields");
+  }
+  const newUser = await User.create({ name, email, password });
+
+  res.status(201).json({ msg: "Registration successfully", user: newUser });
 };
 const login = async (req: Request, res: Response) => {
-    res.send("Login Route");
-}
+  res.send("Login Route");
+};
+export { register, login };
