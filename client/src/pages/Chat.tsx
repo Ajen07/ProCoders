e5d94198fadd3a202e6e44e5ec16687ec627dee0
Saswat Samal <saswat1091@outlook.com>
2024-03-16 22:47:53 +0530

    update chat page

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Search, SendHorizonal } from "lucide-react"
@@ -51,8 +52,8 @@ const Chat = () => {
       </div>
 
       <div className="bg-sky-50 dark:bg-slate-900 flex flex-col flex-grow relative">
-        <div className="flex relative top-0 w-full py-3 px-4 bg-white h-20 dark:bg-slate-800">
-        <div className="flex gap-4">
+        <div className="flex relative top-0 w-full py-3 px-4 bg-white h-18 dark:bg-slate-800 border-b-2">
+          <div className="flex gap-4">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
@@ -64,7 +65,26 @@ const Chat = () => {
 
           </div>
         </div>
-        <ScrollArea className="w-full px-4 py-2 h-full">
+        <ScrollArea className="w-full px-4 py-2 h-full relative">
+          {/* message from the other user */}
+          <Card className="max-w-lg my-1">
+            <CardContent className="py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus amet, necessitatibus iste minus ipsum repellendus consequuntur, excepturi animi nostrum quos consectetur maxime placeat nobis eius unde fugit. Minus velit similique blanditiis
+            </CardContent>
+            <CardFooter>
+              <small>{(new Date()).toLocaleTimeString()}</small>
+            </CardFooter>
+          </Card>
+
+          {/* message from us */}
+          <Card className="max-w-lg my-1 bg-blue-500 text-white dark:bg-blue-950 absolute right-0 mx-4">
+            <CardContent className="py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ducimus amet, necessitatibus iste minus ipsum repellendus consequuntur, excepturi animi nostrum quos consectetur maxime placeat nobis eius unde fugit. Minus velit similique blanditiis
+            </CardContent>
+            <CardFooter>
+              <small>{(new Date()).toLocaleTimeString()}</small>
+            </CardFooter>
+          </Card>
         </ScrollArea>
 
         <form className="flex gap-2 bottom-0 px-2 pb-6">
