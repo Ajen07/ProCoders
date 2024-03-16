ce78132429ac750f9819f937f8e3587b1f713ab9
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-16 12:24:24 +0530

    login and register route added

---
+++ server/src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { register, login } from "../controllers/auth.controller";
+
+const router = express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+
+export default router;
