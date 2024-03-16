984ff4d9892fd83c11ebf5f54411e5e3d214ee6f
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-16 12:23:22 +0530

    custom errors added

---
+++ server/src/errors/conflict.error.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+class ConflictError extends Error {
+  statusCode: number;
+  constructor(message: string) {
+    super(message);
+    this.statusCode = StatusCodes.CONFLICT;
+  }
+}
+
+export default ConflictError;
---
+++ server/src/errors/index.ts
@@ -0,0 +1,11 @@
+import BadRequestError from "./badRequest.error";
+import NotFoundError from "./notFound.error";
+import UnAuthenticatedError from "./unAuthenticated.error";
+import UnAuthorizedError from "./unAuthorized.error";
+
+export {
+    BadRequestError,
+    NotFoundError,
+    UnAuthenticatedError,
+    UnAuthorizedError,
+};
